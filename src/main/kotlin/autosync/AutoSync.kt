e99c7b1594fedc927778c655418b052ea604e552
Boon Hian Tek <boonhian.tek@sc.com>
2016-07-28 11:57:35 +0800

    Added capability to load from config file.

--- src/main/kotlin/autosync/AutoSync.kt
+++ src/main/kotlin/autosync/AutoSync.kt
@@ -1,16 +1,29 @@
 package com.sc.boonatsc.autosync
 
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.PropertySource
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer
+import org.springframework.stereotype.Component
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
+@PropertySource("classpath:/config.local.propertiess", ignoreResourceNotFound = true)
 open class Application {
     companion object {
         @JvmStatic fun main(args: Array<String>) {
             SpringApplication.run(Application::class.java, *args)
         }
+    }
+
+    @Bean open fun propertySourcesPlaceholderConfigurer(): PropertySourcesPlaceholderConfigurer {
+        val configurer = PropertySourcesPlaceholderConfigurer()
+        configurer.setNullValue("@null")
+        return configurer
     }
 }
 
@@ -20,5 +33,13 @@ class AutoSyncController {
     @RequestMapping("/register")
     fun register(): String {
         return "hello world"
+    }
+}
+
+@Component
+class DoOnStartup {
+    @Value("\${auto-sync.server:@null}") var serverAddress: String? = null
+    @PostConstruct fun init() {
+        print(serverAddress)
     }
 }
