3c993023c5d17dfe476f4b920e07b0c9934c0f72
mantis78 <boon.tek@gmail.com>
2016-08-01 23:09:20 +0800

    [BT] Refactor CheckvistTracker with support from test.

--- src/main/kotlin/tormonk/CheckvistTracker.kt
+++ src/main/kotlin/tormonk/CheckvistTracker.kt
@@ -4,6 +4,7 @@ import com.beust.klaxon.*
 import com.github.kittinunf.fuel.core.ResponseDeserializable
 import com.github.kittinunf.fuel.httpGet
 import com.github.kittinunf.result.Result
+import org.slf4j.LoggerFactory
 import org.springframework.stereotype.Component
 import java.io.InputStream
 import java.lang.Long.parseLong
@@ -14,6 +15,7 @@ class CheckvistTracker {
         val specialChecklistId: Long = 569126
         val checkvistService = CheckvistService(specialChecklistId)
         val getTasksUrl = "${CheckvistService.checklistBaseUrl}/${specialChecklistId}/tasks.json"
+        val LOG = LoggerFactory.getLogger(CheckvistTracker::class.java.name)
     }
 
     class Deserializer : ResponseDeserializable<JsonArray<JsonObject>> {
@@ -21,42 +23,45 @@ class CheckvistTracker {
     }
 
     fun getLastUpdateTime(): Long? {
-        val lastUpdateTime = checkvistService?.remote<Long?>(fun(token): Long? {
+        return checkvistService?.remote<Long?>(fun(token): Long? {
             println("Using token: ${token}")
             val (request, response, result) = getTasksUrl.httpGet(listOf("token" to token, "with_notes" to true)).responseObject(Deserializer())
 
-            //do something with response
-            var jsonArr: JsonArray<JsonObject>? = null
-            when (result) {
-                is Result.Failure -> {
-                    println("Got error: " + result.error)
-                }
-                is Result.Success -> {
-                    jsonArr = result.value
-                }
+            if (result is Result.Failure) {
+                LOG.error("Remote service [$getTasksUrl] failed.", result.error.exception)
+                return null
+            }
+            val jsonArr: JsonArray<JsonObject> = result.get()
+
+            val lastUploadedJsonObject = (jsonArr as JsonArray<JsonObject>).filter {
+                "Last Uploaded by tormonk".equals(it.string("content"))
             }
 
-            if (jsonArr != null) {
-                val lastUploadedJsonObject = (jsonArr as JsonArray<JsonObject>).filter {
-                    "Last Uploaded by tormonk".equals(it.string("content"))
-                }
+            if (lastUploadedJsonObject.size < 1) {
+                LOG.error("Failed to load proper task object from JSON.")
+                return null
+            }
+            val notesJsonArr = lastUploadedJsonObject[0].array<JsonObject>("notes")
 
-                val notesJsonArr = lastUploadedJsonObject[0].array<JsonObject>("notes")
-                if (notesJsonArr != null) {
-                    val noteObj = notesJsonArr[0]?.obj("note")
-                    print("WHAT WE WANT[" + noteObj?.string("comment") + "]")
+            if (notesJsonArr == null) {
+                LOG.error("Failed to find note object arrau from JSON.")
+                return null
+            }
+            val noteObj = notesJsonArr[0]?.obj("note")
 
-                    return parseLong(noteObj?.string("comment"))
-                } else {
-                    return null
-                }
-            } else {
+            if (noteObj == null) {
+                LOG.error("Failed to identify expected note object from JSON.")
                 return null
             }
 
-            return null
-        })
+            val commentString = noteObj.string("comment")
 
-        return lastUpdateTime
+            try {
+                return parseLong(commentString)
+            } catch (e: Exception) {
+                LOG.error("Failed to parse comment string [${commentString}].", e)
+                return null
+            }
+        })
     }
 }
