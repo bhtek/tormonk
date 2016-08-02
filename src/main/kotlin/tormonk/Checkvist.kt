4c628d11271ff0b649b43eebdfd15cc524e93c50
mantis78 <boon.tek@gmail.com>
2016-08-02 23:14:45 +0800

    [BT] Add refresh token capability.

--- src/main/kotlin/tormonk/Checkvist.kt
+++ src/main/kotlin/tormonk/Checkvist.kt
@@ -1,12 +1,20 @@
 package tormonk
 
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import org.slf4j.LoggerFactory
+import java.io.File
 import java.util.*
 
 class CheckvistService(checklistId: Long) {
     companion object {
+        val LOG = LoggerFactory.getLogger(CheckvistService.javaClass)
         val baseUrl = "https://checkvist.com"
-        val checklistBaseUrl = "${CheckvistService.baseUrl}/checklists"
+        val checklistBaseUrl = "${baseUrl}/checklists"
+        // e.g. https://checkvist.com/auth/refresh_token.json?old_token=jX0MvhozWsWEsj&with_notes=true
+        val refreshTokenBaseUrl = "${baseUrl}/auth/refresh_token.json"
         val tokenExpiry: Long = 1000 * 60 * 60 * 23
+        val tokenFile = File("${System.getProperty("user.home")}/.tormonk.token")
     }
 
     private var lastActivity: Long = 0
@@ -15,15 +23,33 @@ class CheckvistService(checklistId: Long) {
                 field = value
         }
 
-    private var token: String = login()
+    private var token: String
 
-    fun login(): String {
-        return "7P35qpoIURdwak6eT0oGHKZk9G9Pay"
+    init {
+        if (!tokenFile.exists()) {
+            throw RuntimeException("Expected an existing token but found none.")
+        }
+
+        lastActivity = tokenFile.lastModified()
+        token = tokenFile.readText().trim()
+    }
+
+    fun refreshToken(): String {
+        val (request, response, result) = "${refreshTokenBaseUrl}".httpGet(Arrays.asList("old_token" to token)).responseString()
+        if (result is Result.Failure) {
+            throw RuntimeException("Refresh token failed.", result.error)
+        }
+
+        val token = result.get().replace(Regex("\""), "")
+        LOG.info("Successfully retrieved refreshed token[${token}].")
+
+        return token
     }
 
     fun <T> remote(body: (String) -> T): T {
         if (System.currentTimeMillis() - lastActivity > tokenExpiry) {
-            token = login()
+            token = refreshToken()
+            tokenFile.writeText(token)
         }
 
         return body(token)
