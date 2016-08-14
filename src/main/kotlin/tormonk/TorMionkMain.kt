8c18346b5706422321f8413bd276d306737bfbf1
mantis78 <boon.tek@gmail.com>
2016-08-14 22:41:54 +0800

    [BT] Add scheduling.

--- src/main/kotlin/tormonk/TorMionkMain.kt
+++ src/main/kotlin/tormonk/TorMionkMain.kt
@@ -1,15 +1,18 @@
 package tormonk
 
+import org.slf4j.LoggerFactory
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.context.annotation.ComponentScan
 import org.springframework.context.annotation.PropertySource
+import org.springframework.scheduling.annotation.EnableScheduling
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
-import javax.annotation.PostConstruct
 import javax.annotation.Resource
 
 @SpringBootApplication
 @ComponentScan("tormonk")
+@EnableScheduling
 @PropertySource("classpath:/config.local.properties", ignoreResourceNotFound = true)
 open class TorMonkApplication {
     companion object {
@@ -20,11 +23,18 @@ open class TorMonkApplication {
 }
 
 @Component
-class DoOnStartup {
+class Scheduled {
+    companion object {
+        val LOG = LoggerFactory.getLogger(tormonk.Scheduled::class.java.name)
+    }
+
     @Resource lateinit var checkvistTracker: CheckvistTracker
     @Resource lateinit var showRss: ShowRss
 
-    @PostConstruct fun init() {
+    @Scheduled(cron = "0 * * * * *")
+    fun checkForTorrents() {
+        LOG.info("Begin check.")
+
         val allTasks = checkvistTracker.getAllTasks() ?: return
         val lastUpdateTime = checkvistTracker.getLastUpdateTime(allTasks) ?: return
         checkvistTracker.processTasks(allTasks)
