18d9e4e2544d5a1bdb7ab3ae38907420a7a67af3
mantis78 <boon.tek@gmail.com>
2016-11-03 19:34:16 +0800

    [BT] Adjust to run on local cubox.

--- src/main/kotlin/tormonk/CheckvistTracker.kt
+++ src/main/kotlin/tormonk/CheckvistTracker.kt
@@ -141,7 +141,7 @@ class CheckvistTracker {
             val link = noteObj.string("comment")
 
             // ssh 101.100.161.164 transmission-remote -n 'transmission:password' -w /mnt/nas/Videos/ -a "magnet:?xt=urn:btih:29238E90C2D155B54540B426B0B2F9E5045DC8BB"
-            Runtime.getRuntime().exec("ssh 101.100.161.164 transmission-remote -n 'transmission:password' -w /mnt/nas/Videos/ -a '${link}'")
+            Runtime.getRuntime().exec("transmission-remote -n 'transmission:password' -w /mnt/nas/Videos/ -a '${link}'")
             LOG.info("Sent torrent[${toTorrentTask.string("content")}] to home.")
         }
 
